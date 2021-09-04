9de7a8461cc442f4bfca0a34471612b751b2b478
SuperJaba2000 <superjaba2000@gmail.com>
2021-09-04 20:37:24 +0300

    ..

--- mini-warriors/content/player.js
+++ mini-warriors/content/player.js
@@ -1,3 +1,25 @@
 class Player{
 	
+    lvl = 1;
+	
+	maxHealth = 35;
+	health = 35;
+	isDead = false;
+	speed = 1.0;
+	attack = 15;
+	
+	x = 2;
+	y = 2;
+	
+	constructor(){}
+			
+    damage(amout){
+        if(this.health <= 0){
+			this.isDead = true;
+		}else{
+			this.health -= amout;
+							
+			if(this.health <= 0)this.isDead = true;
+	    }
+	}
 }
